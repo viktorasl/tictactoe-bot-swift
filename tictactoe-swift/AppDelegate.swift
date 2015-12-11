22de5813d87ec27efb742ed6dfbd7e480c3544aa
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-12-12 00:21:14 +0200

    Initial Commit

---
+++ tictactoe-swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tictactoe-swift
+//
+//  Created by Viktoras Laukevičius on 12/12/15.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
