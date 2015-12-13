ba7d71f23404eddf18b4c3f1864a6a0bd2964d34
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-12-13 23:08:01 +0200

    Decoding/encoding generated tests

--- TictactoeTests/TictactoeTests.swift
+++ TictactoeTests/TictactoeTests.swift
@@ -10,23 +10,13 @@ import XCTest
 import SwiftCheck
 
 extension Player: Arbitrary {
-    
     public static var arbitrary: Gen<Player> {
         return Gen.oneOf([Gen.pure(Player.X), Gen.pure(Player.O)])
     }
 }
 
-extension Board: Arbitrary {
-    public static func create(x : Int) -> Board {
-        return Board(fields: [])
-    }
-    public static var arbitrary: Gen<Board> {
-        return Board.create <^> Int.arbitrary
-    }
-}
-
-extension BoardField : Arbitrary {
-    public static func create(x : UInt) -> UInt -> Player -> BoardField {
+extension BoardField: Arbitrary {
+    public static func create(x: UInt) -> UInt -> Player -> BoardField {
         return { y in
             { player in
                 BoardField(x: x % 3, y: y % 3, player: player)
@@ -38,11 +28,37 @@ extension BoardField : Arbitrary {
     }
 }
 
+extension Board: Arbitrary {
+    public static func create(count: UInt) -> Board {
+        return Board(fields: [0..<(count%10)].map{ _ in BoardField.arbitrary.generate })
+    }
+    public static var arbitrary: Gen<Board> {
+        return Board.create <^> UInt.arbitrary
+    }
+}
+
+extension BoardField: Equatable { }
+public func ==(left: BoardField, right: BoardField) -> Bool {
+    return left.x == right.x &&
+        left.y == right.y &&
+        left.player == right.player
+}
+
+func ==(left: Board, right: Board) -> Bool {
+    return left.fields == right.fields
+}
+
 class TictactoeTests: XCTestCase {
     
     func testExample() {
         property("All board fields has valid coordinates") <- forAll{ (field: BoardField) in
             return field.x <= 2 && field.x >= 0 && field.y <= 2 && field.y >= 0
+        }
+        property("Decoded bencode list encoded board is that board") <- forAll{ (board: Board) in
+            return parseBencodeList(encodeBencodeList(board))! == board
+        }
+        property("Decoded bencode dictionary encoded board is that board") <- forAll{ (board: Board) in
+            return parseBencodeDict(encodeBencodeDict(board))! == board
         }
     }
     
