a6dd747e30ac0b96b5b2110945c9a27d6d9d08cf
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-12-13 13:18:45 +0200

    Encoding bencode dict

--- tictactoe-swift/encode.swift
+++ tictactoe-swift/encode.swift
@@ -12,6 +12,15 @@ private func encodeField(field: BoardField) -> String {
     return "d1:v1:\(field.player.rawValue)1:xi\(field.x)e1:yi\(field.y)ee"
 }
 
+private func encodeFieldDict(idx: Int, field: BoardField) -> String {
+    let idxTxt = "\(idx)"
+    return "\(idxTxt.characters.count):\(idxTxt)\(encodeField(field))"
+}
+
 func encodeBencodeList(board: Board) -> String {
     return "l" + board.map{ encodeField($0) }.joinWithSeparator("") + "e"
+}
+
+func encodeBencodeDict(board: Board) -> String {
+    return "d" + board.enumerate().map{ encodeFieldDict($0, field: $1) }.joinWithSeparator("") + "e"
 }
